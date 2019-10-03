82b3876047cc76ef203e46418721b2da5fe7a343
Ramiro <ramiropacheco@outlook.com>
2019-10-03 17:42:35 -0300

    redireccione un index en el navbar y agregue un session destroy para el logout

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header class="fixed-top">
     <nav class="navbar navbar-expand-lg navbar-light bg-dark "  >
-      <a class="navbar-brand text-secondary " href="">Connection</a>
+      <a class="navbar-brand text-secondary " href="index.php">Connection</a>
       <div class="collapse navbar-collapse " id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto  ">
           <li class="nav-item">
@@ -24,10 +24,12 @@
             <a class="nav-link text-secondary" href="formularioContacto.php">Contacto</a>
           </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2 bg-dark" type="search" placeholder="Buscar contenido" aria-label="Search">
-          <button class="btn btn-outline-light my-2 my-sm-0 botonLogin" type="submit"><a href="formularioLogin.php">Login</a></button>
-        </form>
+        <?php if(!isset($_SESSION["email"])) : ?>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2 bg-dark" type="search" placeholder="Buscar contenido" aria-label="Search">
+            <button class="btn btn-outline-light my-2 my-sm-0 botonLogin" type="submit"><a href="formularioLogin.php">Login</a></button>
+          </form>
+        <?php endif ?>
       </div>
     </nav>
   </header>
